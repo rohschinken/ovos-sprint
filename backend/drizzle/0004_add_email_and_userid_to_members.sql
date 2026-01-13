69ca4bf7536e3f06751fb0bbca6a8e4563c7ca6f
Andreas Friedl <af@ovos.at>
2026-01-13 13:07:54 +0100

    fix: add missing statement brakpoint in drizzle migration file

--- backend/drizzle/0004_add_email_and_userid_to_members.sql
+++ backend/drizzle/0004_add_email_and_userid_to_members.sql
@@ -1,3 +1,4 @@
 -- Add email and userId columns to team_members table
 ALTER TABLE `team_members` ADD `user_id` integer REFERENCES users(id) ON DELETE SET NULL;
+--> statement-breakpoint
 ALTER TABLE `team_members` ADD `email` text;
